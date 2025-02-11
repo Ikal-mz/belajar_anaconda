cca114c5dc4c7535ad6279c9423d101fc3712ef0
ikal-mz <mzulfikarb11@gmail.com>
2025-02-11 21:31:24 +0900

    anaconda firs commit

---
+++ .ipynb_checkpoints/main-checkpoint.py
@@ -0,0 +1,6 @@
+print("haloo")
+
+for i in range(1,11):
+    print(f"perulangan ke : {i}")
+    i += 1
+    
---
+++ belajarspider.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+print("coba cetak")
+
+a = 98
+b = 7
+hasil = a*b
+print(f"hasil dari {a} * {b} : {hasil}")
+
+c = [1,2,3,4]
+d = ['1','2','3','4']
